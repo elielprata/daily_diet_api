b171ef2b2850d4655b4667b2a82f0aa57178c0d5
Eliel Martins <elieuprata@gmail.com>
2023-05-04 19:57:37 -0300

    use try catch

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -6,23 +6,27 @@ import bcrypt from 'bcrypt'
 
 export async function users(app: FastifyInstance) {
   app.post('/', async (request, reply) => {
-    const createUserSchema = z.object({
-      name: z.string(),
-      email: z.string().email('invalid email'),
-      password: z.string(),
-    })
+    try {
+      const createUserSchema = z.object({
+        name: z.string(),
+        email: z.string().email('invalid email'),
+        password: z.string(),
+      })
 
-    const { name, email, password } = createUserSchema.parse(request.body)
+      const { name, email, password } = createUserSchema.parse(request.body)
 
-    const passwordHash = await bcrypt.hash(password, 6)
+      const passwordHash = await bcrypt.hash(password, 6)
 
-    await knex('users').insert({
-      id: randomUUID(),
-      name,
-      email,
-      password: passwordHash,
-    })
+      await knex('users').insert({
+        id: randomUUID(),
+        name,
+        email,
+        password: passwordHash,
+      })
 
-    return reply.status(201).send()
+      return reply.status(201).send()
+    } catch (error) {
+      reply.status(400).send({})
+    }
   })
 }
