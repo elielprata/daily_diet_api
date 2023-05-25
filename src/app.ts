5b594b1ba3d8ab0078bd5e77895ea5b1d7201b15
Eliel Martins <elieuprata@gmail.com>
2023-05-25 08:47:57 -0300

    register JWT

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
 import fastify from 'fastify'
+import jwt from '@fastify/jwt'
+
 import { users } from './routes/users'
 import { sessions } from './routes/sessions'
+import { env } from './env'
 
 export const app = fastify()
 
+app.register(jwt, {
+  secret: env.JWT_SECRET,
+})
+
 app.register(sessions)
 app.register(users, { prefix: '/users' })
+app.register(users, { prefix: '/meals' })
