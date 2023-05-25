839d57e6e9c19c819b5d7ccdc1ec2642c0107178
Eliel Martins <elieuprata@gmail.com>
2023-05-25 14:44:27 -0300

    finalize meals route

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -1,10 +1,33 @@
 import { FastifyInstance } from 'fastify'
 import { ZodError, z } from 'zod'
 import { knex } from '../database'
+import { randomUUID } from 'node:crypto'
 
 export async function meals(app: FastifyInstance) {
   app.addHook('preHandler', async (request) => {
     await request.jwtVerify()
+  })
+
+  app.get('/', async (request, reply) => {
+    const meals = await knex('meals')
+      .select('*')
+      .where({ user_id: request.user.sub })
+
+    return reply.status(200).send({ meals })
+  })
+
+  app.get('/:id', async (request, reply) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const meal = await knex('meals')
+      .where({ id, user_id: request.user.sub })
+      .first()
+
+    return reply.status(200).send({ meal })
   })
 
   app.post('/', async (request, reply) => {
@@ -21,6 +44,7 @@ export async function meals(app: FastifyInstance) {
       )
 
       await knex('meals').insert({
+        id: randomUUID(),
         name,
         description,
         date: dateTime,
@@ -34,6 +58,69 @@ export async function meals(app: FastifyInstance) {
         return reply.status(403).send(error.issues)
       }
 
+      console.log(error)
+    }
+  })
+
+  app.put('/:id', async (request, reply) => {
+    try {
+      const paramsSchema = z.object({
+        id: z.string().uuid(),
+      })
+
+      const { id } = paramsSchema.parse(request.params)
+
+      const meal = await knex('meals')
+        .where({
+          id,
+          user_id: request.user.sub,
+        })
+        .first()
+
+      if (!meal) {
+        return reply.status(404).send()
+      }
+
+      const mealsSchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        dateTime: z.string(),
+        isDiet: z.coerce.boolean(),
+      })
+
+      const { name, description, dateTime, isDiet } = mealsSchema.parse(
+        request.body,
+      )
+
+      await knex('meals').where({ id }).update({
+        name,
+        description,
+        date: dateTime,
+        is_diet: isDiet,
+      })
+
+      return reply.status(204).send()
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return reply.status(403).send(error.issues)
+      }
+
+      console.log(error)
+    }
+  })
+
+  app.delete('/:id', async (request, reply) => {
+    try {
+      const paramsSchema = z.object({
+        id: z.string().uuid(),
+      })
+
+      const { id } = paramsSchema.parse(request.params)
+
+      await knex('meals').where({ id, user_id: request.user.sub }).del()
+
+      return reply.status(204).send()
+    } catch (error) {
       console.log(error)
     }
   })
